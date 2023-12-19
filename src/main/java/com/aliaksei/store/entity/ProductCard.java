646e45ac355f07f38f5098c2b32cc1ba08ca564b
Aliaksei <kulevetsav@gmail.com>
2023-12-19 11:20:54 +0100

    Connecting a database to a project

--- src/main/java/com/aliaksei/store/entity/ProductCard.java
+++ src/main/java/com/aliaksei/store/entity/ProductCard.java
@@ -15,7 +15,6 @@ import java.util.List;
 public class ProductCard {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "card_id")
     private int productCardId;
 
